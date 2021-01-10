0d95ba52b7c33553e348279b1d2de5950e1ffb4d
HellAmbro <frambrosini1998@gmail.com>
2021-01-10 01:57:57 +0100

    Initial commit

--- Trading212API/README.md
+++ Trading212API/README.md
@@ -1,5 +1,8 @@
-from trading212 import Trading212
+# Trading212 API
+**Unofficial** API for Trading212 broker. 
 
+###Usage
+````
 trading212 = Trading212('your_email', 'password', mode=Mode.DEMO, save_cookies=True)
 
 market_order = trading212.execute_order(MarketOrder(instrument_code="AMZN_US_EQ", quantity=2))
@@ -17,3 +20,4 @@ stop_limit = trading212.execute_order(
 trading212.get_funds()
 trading212.get_orders()
 trading212.cancel_order(14238041)
+````
---
+++ Trading212API/example.py
@@ -0,0 +1,19 @@
+from Trading212API import Trading212
+
+trading212 = Trading212('your_email', 'password', mode=Mode.DEMO, save_cookies=True)
+
+market_order = trading212.execute_order(MarketOrder(instrument_code="AMZN_US_EQ", quantity=2))
+
+limit_order = trading212.execute_order(
+    LimitOrder(instrument_code="AMZN_US_EQ", quantity=2, limit_price=3000, time_validity=TimeValidity.DAY))
+
+stop_order = trading212.execute_order(
+    StopOrder(instrument_code="AMZN_US_EQ", quantity=2, stop_price=4000, time_validity=TimeValidity.GOOD_TILL_CANCEL))
+
+stop_limit = trading212.execute_order(
+    StopLimitOrder(instrument_code="AMZN_US_EQ", quantity=2, limit_price=3000, stop_price=4000,
+                   time_validity=TimeValidity.GOOD_TILL_CANCEL))
+
+trading212.get_funds()
+trading212.get_orders()
+trading212.cancel_order(14238041)
