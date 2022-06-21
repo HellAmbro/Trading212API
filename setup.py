9bec10673035dd301be8c85576534cca3a063e09
HellAmbro <frambrosini1998@gmail.com>
2022-06-21 22:41:56 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytrading212",
-    version="0.1.8",
+    version="0.1.9",
     author="HellAmbro",
     author_email="frambrosini1998@gmail.com",
     description="Unofficial Trading212 API",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/HellAmbro/Trading212API",
     packages=setuptools.find_packages(),
-    install_requires=[
+    dependencies=[
         'selenium',
     ],
     classifiers=[
